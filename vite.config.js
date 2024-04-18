8b2de918f72389d798dbff349892cf459a264239
Arzoyan <ruben.arzoyan@gmail.com>
2024-04-18 23:17:28 +0400

    updates for deployement

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,5 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-    plugins: [react()],
-    build: {
-        manifest: true,
-        rollupOptions: {
-            input: './src/main.jsx', // Set the bundle's entry point to src/main.jsx
-        },
-    }
+    plugins: [react()]
 });
